8f00534ceec0658a3ddb0705a44eb2e908d80056
Sam Mikes <smikes@cubane.com>
2014-09-08 07:36:55 -0600

    start writing extension

--- test/simple.js
+++ test/simple.js
@@ -4,9 +4,24 @@
 'use strict';
 
 var assert = require('assert'),
-    adapter = require('..');
+    adapter = require('..'),
+    Mocha = require('mocha'),
+    mocha = new Mocha(),
+    test262Bdd;
 
 it('loads the test suite and runs a test', function () {
     adapter();
     assert.ok(true);
 });
+
+test262Bdd = function (suite) {
+    suite.on('pre-require', function (context, file, mocha) {
+
+        context.it = function (title, fn) {
+        };
+    });
+};
+
+Mocha.interfaces['test262-bdd'] = test262Bdd;
+mocha.ui('test262-bdd');
+
