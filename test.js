70e02a6a5372b637899603905b28f51e523b2505
Sam Mikes <smikes@cubane.com>
2014-09-28 09:04:32 -0600

    failing test, need to figure out tdd/test262 simul support

--- test.js
+++ test.js
@@ -6,12 +6,22 @@ var assert = require('assert'),
 Mocha.interfaces.test262 = adapter.test262;
 
 mocha = new Mocha();
+
 mocha.loadFiles = adapter.loadFiles;
+
+mocha.suite = new Mocha.Suite(undefined, 'parent');
+var parentSuite = mocha.suite;
+mocha.suite = new Mocha.Suite(mocha.suite, 'standard tests');
+mocha.ui('bdd');
+mocha.files = ['test/simple.js'];
+
+mocha.suite = new Mocha.Suite(parentSuite, 'test262 fixtures');
 mocha.ui('test262');
 
-mocha.files = ['test/fixtures/S7.6_A1.2_T1.js',
+mocha.files.push('test/fixtures/S7.6_A1.2_T1.js',
                'test/fixtures/syntaxerror.js',
-               'test/fixtures/dumb-async.js'];
+               'test/fixtures/dumb-async.js',
+               'test/fixtures/failing.js');
 
 
 mocha.run();
---
+++ test/fixtures/failing.js
@@ -0,0 +1,11 @@
+// Copyright 2014 Sam Mikes
+// This file is governed by the BSD license in the LICENSE file.
+
+/*---
+description: non-negative test that fails
+info: failing test
+es5id: 25.4.4.1_A2.3_T3
+author: Sam Mikes
+---*/
+
+throw new Error("failing test");
